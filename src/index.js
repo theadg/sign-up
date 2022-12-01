1e98ab2d3cba15fb820a0c2a0ae3e3ada07ab624
theadg <deguzmanbernardandrew@gmail.com>
2022-12-01 17:17:24 +0800

    Complete validation

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ const zipInput = document.querySelector('#zip');
 const pwInput = document.querySelector('#pw');
 const confirmPwInput = document.querySelector('#confirmPw');
 
+const inputControls = [
+  mailInput,
+  countryInput,
+  zipInput,
+  pwInput,
+  confirmPwInput,
+];
+
 // Errors
 const mailError = document.querySelector('#mailError');
 const countryError = document.querySelector('#countryError');
@@ -14,20 +22,44 @@ const zipError = document.querySelector('#zipError');
 const pwError = document.querySelector('#pwError');
 const confirmPwError = document.querySelector('#confirmPwError');
 
+// button
+const submitButton = document.querySelector('#submit');
+
+// final
 // validation
-const validateInput = (inputField, inputName, inputErrorField) => {
+const validation = (element) => element.validity.valid === true;
+
+const checkIfValid = () => {
+  if (inputControls.every(validation)) submitButton.disabled = false;
+  else {
+    submitButton.disabled = true;
+    submitButton.setAttribute('title', 'Complete all Input fields first');
+  }
+};
+
+const validateInput = (
+  inputField,
+  inputName,
+  inputErrorField,
+  inputErrorMessage = ''
+) => {
   if (inputField.validity.typeMismatch) {
-    inputErrorField.textContent = `Please Enter a valid ${inputName}`;
+    inputErrorField.textContent = `Enter a valid ${inputName}`;
   } else if (inputField.validity.tooShort) {
-    inputErrorField.textContent = `Characters should be at least ${inputField.minLength}`;
+    inputErrorField.textContent = `There should be at least ${inputField.minLength} characters`;
     console.log('hello error?');
   } else if (inputField.validity.tooLong) {
     inputErrorField.textContent = `Characters should be at most ${inputField.maxLength}`;
   } else if (inputField.validity.valueMissing) {
     inputErrorField.textContent = `Input should be not empty`;
+  } else if (inputField.validity.patternMismatch) {
+    inputErrorField.textContent = inputErrorMessage;
+    console.log('he?');
   } else {
     inputErrorField.textContent = ``;
   }
+
+  checkIfValid();
 };
 
 mailInput.oninput = () => {
@@ -35,17 +67,49 @@ mailInput.oninput = () => {
 };
 
 countryInput.oninput = () => {
-  validateInput(countryInput, 'Email', countryError);
+  validateInput(countryInput, 'Country', countryError);
 };
 
 zipInput.oninput = () => {
-  validateInput(zipInput, 'Email', zipError);
+  validateInput(zipInput, 'Zip Code', zipError, 'ddddd or ddddd-dddd format');
 };
 
 pwInput.oninput = () => {
-  validateInput(pwInput, 'Email', pwError);
+  validateInput(
+    pwInput,
+    'Password',
+    pwError,
+    'Password should contain at least: one uppercase letter, one lowercase letter, one symbol. Minimum 8 characters'
+  );
 };
 
 confirmPwInput.oninput = () => {
-  validateInput(confirmPwInput, 'Email', confirmPwError);
+  validateInput(
+    confirmPwInput,
+    'password',
+    confirmPwError,
+    'Password should contain at least: one uppercase letter, one lowercase letter, one symbol. Minimum 8 characters'
+  );
 };
+
+submitButton.onclick = (e) => {
+  if (
+    pwInput.value !== confirmPwInput.value &&
+    pwInput.value !== '' &&
+    confirmPwInput !== ''
+  ) {
+    pwError.textContent = 'Passwords must match';
+    pwInput.classList.add('invalid');
+    confirmPwInput.classList.add('invalid');
+  } else {
+    alert('Form Submitted Successfully');
+    const allInput = Array.from(document.querySelectorAll('input'));
+    allInput.forEach((input) => {
+      input.value = '';
+    });
+  }
+
+  e.preventDefault();
+};
+
+window.onload = () => checkIfValid();
